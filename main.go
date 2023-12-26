a4928052919ca08541dac9cfdb5dddf3a31e4b37
tde-nico <101901057+tde-nico@users.noreply.github.com>
2023-12-26 23:31:32 +0100

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"go-API_Echo_test/cmd/api/handlers"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/health-check", handlers.HealthCheckHandler)
+	e.GET("/posts", handlers.PostIndexHandler)
+	e.GET("/post/:id", handlers.PostSingleHandler)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
